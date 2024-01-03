d421c65744a03a178ddca7378259bb2d98c2fa14
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-01-03 16:33:49 +0100

    Working on the nav_bar component

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const set_nav_bar = () => {
-    if (!document.getElementById('nav_bar')?.innerHTML) {
+    const navBarElement = document.getElementById('nav_bar');
+    
+    if (!navBarElement) {
         return;
     }
-    document.getElementById('nav_bar').innerHTML = `<div class="d-flex p-2 justify-content-between position-sticky top-0"
-                                                    style="height: 100px; z-index: 999999; background: gray;">
-                                                    <div class="d-flex flex-sm-col justify-content-center align-items-center gap-3" style="height: 60px;">
-                                                        <img class="h-100" src="assets/icon.png" alt="Finova Bank Logo">
-                                                        <p>Finova Bank</p>
-                                                    </div>
-                                                    <div class="d-flex flex-sm-col justify-content-center align-items-center gap-3">
-                                                        <a href="bankSignUpPage.html" class="text-decoration-none">Create Account</a>
-                                                        <a href="bankSignInPage.html" class="text-decoration-none">Log In</a>
-                                                        <a href="aboutUs.html" class="text-decoration-none">About Us</a>
-                                                    </div>
-                                                </div>`
+
+    navBarElement.innerHTML = `<div class="d-flex p-2 justify-content-between position-sticky top-0"
+                                style="height: 100px; z-index: 999999; background: gray;">
+                                <div class="d-flex flex-sm-col justify-content-center align-items-center gap-3" style="height: 60px;">
+                                    <img class="h-100" src="assets/icon.png" alt="Finova Bank Logo">
+                                    <p>Finova Bank</p>
+                                </div>
+                                <div class='justify-content-center align-items-center h-full position-relative'>
+                                    <div class="responsive hidden d-sm-flex flex-sm-col my-auto justify-content-center align-items-center gap-3">
+                                        <a href="bankSignUpPage.html" class="text-decoration-none">Create Account</a>
+                                        <a href="bankSignInPage.html" class="text-decoration-none">Log In</a>
+                                        <a href="aboutUs.html" class="text-decoration-none">About Us</a>
+                                    </div>
+                                </div>
+                            </div>`;
 }
 
-window.onload = set_nav_bar()
+window.onload = set_nav_bar;
+
 
 const showToast = () => {
     const toast = document.getElementById('toast');
