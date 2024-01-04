e86bbddca3151c99b4b880cba5f437d4f71a633e
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-01-04 01:09:57 +0100

    Added other bank names to the transfer section

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const set_nav_bar = () => {
 
     navBarElement.innerHTML = `<div class="d-flex p-2 justify-content-between position-sticky top-0"
                                 style="height: 100px; z-index: 999999; background: gray;">
-                                <div class="d-flex flex-sm-col justify-content-center align-items-center gap-3" style="height: 60px;">
+                                <div onclick="window.location.href='index.html'" class="cursor-pointer d-flex flex-sm-col justify-content-center align-items-center gap-3" style="height: 60px;">
                                     <img class="h-100" src="assets/icon.png" alt="Finova Bank Logo">
-                                    <p>Finova Bank</p>
+                                    <p class=' text-white'>Finova Bank</p>
                                 </div>
-                                <div class='justify-content-center align-items-center h-full position-relative'>
-                                    <div class="responsive hidden d-sm-flex flex-sm-col my-auto justify-content-center align-items-center gap-3">
-                                        <a href="bankSignUpPage.html" class="text-decoration-none">Create Account</a>
-                                        <a href="bankSignInPage.html" class="text-decoration-none">Log In</a>
-                                        <a href="aboutUs.html" class="text-decoration-none">About Us</a>
+                                <div class='justify-content-center d-flex align-items-center h-full position-relative'>
+                                    <div class="responsiveg text-light h-full hidden d-sm-flex flex-sm-col my-auto justify-content-center align-items-center gap-3">
+                                        <a href="bankSignUpPage.html" class="text-decoration-none text-white">Create Account</a>
+                                        <a href="bankSignInPage.html" class="text-decoration-none text-white">Log In</a>
+                                        <a href="aboutUs.html" class="text-decoration-none text-white">About Us</a>
                                     </div>
                                 </div>
                             </div>`;
